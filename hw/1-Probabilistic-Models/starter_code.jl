f2700548a42efc561d3bbd57ea477fce26c500ae
xavier2933 <102980192+xavier2933@users.noreply.github.com>
2025-01-27 11:07:56 -0700

    hw1 added

--- hw/1-Probabilistic-Models/starter_code.jl
+++ hw/1-Probabilistic-Models/starter_code.jl
@@ -4,10 +4,32 @@ import DMUStudent.HW1
 # Problem 4
 #-------------
 
-# Here is a functional but incorrect answer for the programming question
 function f(a, bs)
-    return a*bs[1]
+
+    aType = eltype(a) # get type of a to return correct type
+    res = Vector{Vector{aType}}()
+
+    for val in bs
+        push!(res, a * val)
+    end
+    @show res
+
+    """
+    ChatGPT told me about this function, liked it more than iterating with for loop
+    Applies max element wise to vector of vectors, returning max of each position
+    in each vector
+    maxRes = reduce(max, res)
+    
+    This forum post says these are equivalent, couldn't find anything good on reduce 
+    so didn't want to use it
+    https://discourse.julialang.org/t/max-of-a-vector/108294/10
+    """
+
+    maxRes = maximum(res)
+
+    return maxRes
 end
+
 
 # You can can test it yourself with inputs like this
 a = [1.0 2.0; 3.0 4.0]
@@ -17,4 +39,4 @@ bs = [[1.0, 2.0], [3.0, 4.0]]
 @show f(a, bs)
 
 # This is how you create the json file to submit
-HW1.evaluate(f, "your.gradescope.email@colorado.edu")
+HW1.evaluate(f, "xavier.okeefe@colorado.edu")
