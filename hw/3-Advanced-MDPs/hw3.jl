ba86fd966595f109620c38f0d70fc06709819a57
xavier2933 <102980192+xavier2933@users.noreply.github.com>
2025-02-17 13:38:31 -0700

    q4

--- hw/3-Advanced-MDPs/hw3.jl
+++ hw/3-Advanced-MDPs/hw3.jl
@@ -48,9 +48,152 @@ function heuristic_policy(m, s)
     return rand(actions(m))
 end
 
-println(actions(m))
+# println(actions(m))
 # This code runs monte carlo simulations: you can calculate the mean and standard error from the results
-results = [rollout(m, heuristic_policy, rand(initialstate(m))) for _ in 1:1000]
+# results = [rollout(m, heuristic_policy, rand(initialstate(m))) for _ in 1:1000]
 
-println("MEAN: ", mean(results))
-println("SEM: ", std(results)/sqrt(length(results)))
+# println("MEAN: ", mean(results))
+# println("SEM: ", std(results)/sqrt(length(results)))
+
+############
+# Question 3
+############
+struct MonteCarloTreeSearch
+    P # problem
+    N # visit counts
+    Q # action value estimates
+    d # depth
+    m # number of simulations
+    c # exploration constant
+    U # value function estimate
+end
+
+function MonteCarloTreeSearch(; P, N, Q, d, m, c, U)
+    return MonteCarloTreeSearch(P, N, Q, d, m, c, U)
+end
+
+function (pi::MonteCarloTreeSearch)(s)
+    for k in 1:pi.m
+        simulate!(pi, s)
+    end
+    return argmax(a -> pi.Q[(s, a)], actions(pi.P))
+end
+
+function simulate!(π::MonteCarloTreeSearch, s, d=π.d)
+    if d ≤ 0
+        return π.U(s)
+    end
+    # println("SIMULATE")
+    P, N, Q, c = π.P, π.N, π.Q, π.c
+    𝒜, γ = actions(P), P.discount
+    if !haskey(N, (s, first(𝒜)))
+        for a in 𝒜
+            N[(s,a)] = 0
+            Q[(s,a)] = 0.0
+        end
+        return π.U(s)
+    end
+    a = explore(π, s)
+    sp, r = @gen(:sp, :r)(P, s, a)
+    # println("Next state: ", sp)
+    # println("Reward: ", r)
+    q = r +γ*simulate!(π, sp, d-1)
+    N[(s,a)] +=1
+    Q[(s,a)] +=(q-Q[(s,a)])/N[(s,a)]
+    return q
+end
+
+
+bonus(Nsa, Ns) = Nsa == 0 ? Inf : sqrt(log(Ns) / Nsa)
+
+function explore(π::MonteCarloTreeSearch, s)
+    𝒜, N, Q, c = actions(π.P), π.N, π.Q, π.c
+    Ns = sum(N[(s,a)] for a in 𝒜)
+    return argmax(a->Q[(s,a)]+ c*bonus(N[(s,a)], Ns), 𝒜)
+end
+
+m = DenseGridWorld(seed=4)
+println(actions(m))
+a = :right
+
+@show S = statetype(m)
+@show A = actiontype(m)
+
+# These would be appropriate containers for your Q, N, and t dictionaries:
+n = Dict{Tuple{S, A}, Int}()
+q = Dict{Tuple{S, A}, Float64}()
+t = Dict{Tuple{S, A, S}, Int}()
+
+# This is an example state - it is a StaticArrays.SVector{2, Int}
+@show s = SA[19,19]
+
+sp, r = @gen(:sp,:r)(m,s,a)
+println("S' = $sp")
+
+
+@show typeof(s)
+@assert s isa statetype(m)
+
+# here is an example of how to visualize a dummy tree (q, n, and t should actually be filled in your mcts code, but for this we fill it manually)
+# q[(SA[1,1], :right)] = 0.0
+# q[(SA[2,1], :right)] = 0.0
+# n[(SA[1,1], :right)] = 1
+# n[(SA[2,1], :right)] = 0
+# t[(SA[1,1], :right, SA[2,1])] = 1
+
+
+π = MonteCarloTreeSearch(
+    P=m, # problem
+    N=n, # visit counts
+    Q=q, # action value estimates
+    d=7, # depth
+    m=100, # number of simulations
+    c=1.0, # exploration constant
+    U=s -> 0.0 # default value function estimate
+)
+@show best_action = π(s)             # Call the instance like a function
+
+# inchrome(visualize_tree(q, n, t, SA[19,19])) # use inbrowser(visualize_tree(q, n, t, SA[1,1]), "firefox") etc. if you want to use a different browser
+
+############
+# Question 4
+############
+
+# A starting point for the MCTS select_action function (a policy) which can be used for Questions 4 and 5
+function select_action(m, s)
+    start = time_ns()
+    S = statetype(m)
+    A = actiontype(m)
+    n = Dict{Tuple{S, A}, Int}()
+    q = Dict{Tuple{S, A}, Float64}()
+
+    # Define the MCTS policy
+    π = MonteCarloTreeSearch(
+        P=m, # problem
+        N=n, # visit counts
+        Q=q, # action value estimates
+        d=10, # depth
+        m=100, # number of simulations
+        c=1.0, # exploration constant
+        U=s -> 0.0 # default value function estimate
+    )
+
+    # Run MCTS iterations
+    for i in 1:1000
+        while time_ns() < start + 40_000_000 # uncomment this line to limit the loop to 40ms
+            simulate!(π, s)
+        end
+        # println("iteration $i")
+    end
+
+    # Select the best action based on Q values
+    best_action = argmax(a -> q[(s, a)], actions(m))
+    println("Best action $best_action")
+    return best_action
+end
+
+
+# @btime select_action(m, SA[35,35]) # you can use this to see how much time your function takes to run. A good time is 10-20ms.
+
+# use the code below to evaluate the MCTS policy
+@show results = [rollout(m, select_action, rand(initialstate(m)), 100) for _ in 1:100]
