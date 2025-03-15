80fa73680fdcf547a4026d16242dfd88ab6edb8f
xavier2933 <102980192+xavier2933@users.noreply.github.com>
2025-03-15 12:46:05 -0600

    runs q3

---
+++ hw/5-Deep-Reinforcement-Learning/q3.jl
@@ -0,0 +1,105 @@
+using DMUStudent.HW5: HW5, mc
+using QuickPOMDPs: QuickPOMDP
+using POMDPTools: Deterministic, Uniform, SparseCat, FunctionPolicy, RolloutSimulator
+using Statistics: mean
+import POMDPs
+using CommonRLInterface
+using Flux
+using CommonRLInterface.Wrappers: QuickWrapper
+
+# The following are some basic components needed for DQN
+
+# Override to a discrete action space, and position and velocity observations rather than the matrix.
+env = QuickWrapper(HW5.mc,
+                   actions=[-1.0, -0.5, 0.0, 0.5, 1.0],
+                   observe=mc->observe(mc)[1:2]
+                  )
+
+function dqn(env)
+    # This network should work for the Q function - an input is a state; the output is a vector containing the Q-values for each action 
+    Q = Chain(Dense(2, 128, relu),
+              Dense(128, length(actions(env))))
+
+    opt = Flux.setup(ADAM(0.0005), Q)
+
+
+    # We can create 1 tuple of experience like this
+    s = observe(env)
+    @show s
+    a_ind = 1 # action index - the index, rather than the actual action itself, will be needed in the loss function
+    r = act!(env, actions(env)[a_ind])
+    sp = observe(env)
+    done = terminated(env)
+
+    experience_tuple = (s, a_ind, r, sp, done)
+
+    # this container should work well for the experience buffer:
+    buffer = [experience_tuple]
+    # you will need to push more experience into it and randomly select data for training
+    
+    reset!(env) # NOTE: after each time the environment reaches a terminal state, you need to reset it
+
+    # this is the fixed target Q network
+    Q_target = deepcopy(Q)
+
+    # create your loss function for Q training here
+    function loss(Q, s, a_ind, r, sp, done)
+        curr = Q(s)[a_ind]
+        if done
+            target = r
+        else
+            target = r + 0.99 * maximum(Q(sp))
+        end
+
+        return (target - curr)^2
+    end
+
+    # select some data from the buffer
+    data = rand(buffer, 10)
+
+    # do your training like this (you may have to adjust some things, and you will have to do this many times):
+    for i in 1:10000
+        Flux.Optimise.train!(loss, Q, data, opt)
+        println("Trained $i times")
+        if i % 50 == 0
+            display(render(env))
+        end
+    end
+
+    # Make sure to evaluate, print, and plot often! You will want to save your best policy.
+    
+    return Q
+end
+
+Q = dqn(env)
+
+HW5.evaluate(s->actions(env)[argmax(Q(s[1:2]))], n_episodes=100) # you will need to remove the n_episodes=100 keyword argument to create a json file; evaluate needs to run 10_000 episodes to produce a json
+
+#----------
+# Rendering
+#----------
+
+# You can show an image of the environment like this (use ElectronDisplay if running from REPL):
+display(render(env))
+
+# The following code allows you to render the value function
+using Plots
+xs = -3.0f0:0.1f0:3.0f0
+vs = -0.3f0:0.01f0:0.3f0
+heatmap(xs, vs, (x, v) -> maximum(Q([x, v])), xlabel="Position (x)", ylabel="Velocity (v)", title="Max Q Value")
+
+
+# function render_value(value)
+#     xs = -3.0:0.1:3.0
+#     vs = -0.3:0.01:0.3
+
+#     data = DataFrame(
+#                      x = vec([x for x in xs, v in vs]),
+#                      v = vec([v for x in xs, v in vs]),
+#                      val = vec([value([x, v]) for x in xs, v in vs])
+#     )
+
+#     data |> @vlplot(:rect, "x:o", "v:o", color=:val, width="container", height="container")
+# end
+
+# display(render_value(s->maximum(Q(s))))
